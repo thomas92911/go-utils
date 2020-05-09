6f8d8d3e68f9074625ad06852fc751dfa6ce46bf
thomas92911 <thomas92911@gmail.com>
2020-05-10 00:33:52 +0800

    move func

--- math.go
+++ math.go
@@ -1,5 +1,7 @@
 package utils
 
+import "encoding/binary"
+
 // Amount represents the base bitcoin monetary unit (colloquially referred
 // to as a `Satoshi').  A single Amount is equal to 1e-8 of a bitcoin.
 type Amount int64
@@ -13,4 +15,33 @@ func Round(f float64) Amount {
 		return Amount(f - 0.5)
 	}
 	return Amount(f + 0.5)
+}
+
+// Int64ToBytes func
+func Int64ToBytes(i int64) []byte {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(i))
+	return buf
+}
+
+// BytesToInt64 func
+func BytesToInt64(buf []byte) int64 {
+	return int64(binary.BigEndian.Uint64(buf))
+}
+
+// Uint64ToBytes func
+func Uint64ToBytes(i uint64) []byte {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], i)
+	return buf[:]
+}
+
+// BytesToUint64 func
+func BytesToUint64(b []byte) uint64 {
+	return binary.BigEndian.Uint64(b)
+}
+
+// Add function to add two uint64 numbers
+func Add(existing, new []byte) []byte {
+	return Uint64ToBytes(BytesToUint64(existing) + BytesToUint64(new))
 }
--- strings.go
+++ strings.go
@@ -1,34 +1,9 @@
 package utils
 
 import (
-	"encoding/binary"
 	"regexp"
 	"strings"
 )
-
-// Int64ToBytes func
-func Int64ToBytes(i int64) []byte {
-	var buf = make([]byte, 8)
-	binary.BigEndian.PutUint64(buf, uint64(i))
-	return buf
-}
-
-// BytesToInt64 func
-func BytesToInt64(buf []byte) int64 {
-	return int64(binary.BigEndian.Uint64(buf))
-}
-
-// Uint64ToBytes func
-func Uint64ToBytes(i uint64) []byte {
-	var buf [8]byte
-	binary.BigEndian.PutUint64(buf[:], i)
-	return buf[:]
-}
-
-// BytesToUint64 func
-func BytesToUint64(b []byte) uint64 {
-	return binary.BigEndian.Uint64(b)
-}
 
 // ElideString func
 func ElideString(s string, maxLen int) string {
@@ -46,11 +21,6 @@ func ElideString(s string, maxLen int) string {
 	s = s[0:maxLen-3] + "..."
 
 	return s
-}
-
-// Add function to add two uint64 numbers
-func Add(existing, new []byte) []byte {
-	return Uint64ToBytes(BytesToUint64(existing) + BytesToUint64(new))
 }
 
 // GetLastNameString func
